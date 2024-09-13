a79b47039e0f30302fa8bb9671b7bc26de51689b
jacostaf10 <jorge.acosta.freire@gmail.com>
2024-09-13 11:48:16 +0200

    Initial Commit

---
+++ RickMorty/RickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApp.swift
+//  RickMorty
+//
+//  Created by Jorge Acosta Freire on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
