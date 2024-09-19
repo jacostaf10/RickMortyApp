10fe3ec32d56db0ce81ca2740818720ba03a8717
jacostaf10 <jorge.acosta.freire@gmail.com>
2024-09-19 10:06:46 +0200

    Locations master-detail first iteration

--- RickMorty/LocationDetail.swift
+++ RickMorty/LocationDetail.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import RickMortyNet
 
 struct LocationDetail: View {
+    let location: Location
+    
+    @State private var residents: [Character] = []
+    
+    private func getResidents() async throws {
+        for resident in location.residents {
+            guard let number = resident.components(separatedBy: "/").last, let index = Int(String(number)) else { continue }
+            let character = try await RMNetwork.shared.getCharacter(index)
+            residents.append(character)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .fontDesign(.rounded)
+            Text(location.type)
+                .font(.title)
+                .fontWeight(.bold)
+                .fontDesign(.rounded)
+            
+            Section {
+                List(residents) { character in
+                    HStack {
+                        AsyncImage(url: URL(string: character.image)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
+                        } placeholder: {
+                            Image(systemName: "person")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        
+                        Text(character.name)
+                    }
+                }
+            } header: {
+                HStack {
+                    Text("Residents")
+                        .font(.title3)
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            
+        }
+        .task {
+            do {
+                try await getResidents()
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
 #Preview {
-    LocationDetail()
+    LocationDetail(location: .test)
 }
--- RickMorty/LocationsView.swift
+++ RickMorty/LocationsView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct LocationsView: View {
+    @Environment(ViewModel.self) var vm
+    @Binding var routes: [Route]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $routes) {
+            List(vm.locations) { location in
+                Button {
+                    routes.append(.location(location))
+                } label: {
+                    VStack(alignment: .leading) {
+                        Text(location.name)
+                        Text(location.type)
+                            .font(.footnote)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .buttonStyle(.plain)
+
+            }
+            .navigationTitle("Locations")
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .location(let location):
+                    LocationDetail(location: location)
+                default: EmptyView()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    LocationsView()
+    LocationsView(routes: .constant([]))
+        .environment(ViewModel())
 }
